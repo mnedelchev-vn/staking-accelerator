2160946054dc09d3cccc14b553c5bcc498af890f
Miroslav Nedelchev <miroslav.nedelchev@dentacoin.com>
2022-06-30 13:01:33 +0300

    fix syntaxis issues

--- StakingAccelerator.sol
+++ StakingAccelerator.sol
@@ -1,10 +1,46 @@
-/**
- *Submitted for verification at optimistic.etherscan.io on 2022-05-18
-*/
-
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.7.6;
-pragma abicoder v2;
+pragma solidity 0.8.4;
+
+
+interface DentacoinToken {
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+}
+
+
+interface ISwapRouter {
+    function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
+
+    struct ExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+        uint160 sqrtPriceLimitX96;
+    }
+}
+
+
+interface IQuoter {
+    function quoteExactInputSingle(
+        address tokenIn,
+        address tokenOut,
+        uint24 fee,
+        uint256 amountIn,
+        uint160 sqrtPriceLimitX96
+    ) external returns (uint256 amountOut);
+}
+
+
+interface StakingProgram {
+    function whitelistedStake(uint256 _tokens_amount, address _staker) external;
+}
+
 
 contract Ownable {
     address public owner;
@@ -22,6 +58,7 @@ contract Ownable {
         if (newOwner != address(0)) owner = newOwner;
     }
 }
+
 
 contract SafeMath {
     /**
@@ -83,40 +120,47 @@ contract SafeMath {
     }
 }
 
+
 contract StakingAccelerator is Ownable, SafeMath {
     uint256 public incentivePercentage = 5;
 
     // DentacoinToken Instance
-    address public token_address;
-    DentacoinToken TokenContract;
+    address public tokenAddress;
+    DentacoinToken tokenContract;
 
     // SwapRouter Instance
-    ISwapRouter SwapRouter;
+    ISwapRouter swapRouter;
 
     // StakingProgram Instance
-    StakingProgram StakingContract;
+    StakingProgram stakingContract;
 
     // Quoter instance
-    IQuoter Quoter;
+    IQuoter quoter;
 
     bool public contractStopped = false;
     uint16 public slippage = 20;
     uint24 public uniswapFee = 3000;
     address public tokenIn;
 
-    constructor(address _token_address, address _tokenIn, address _swap_router_address, address _staking_address, address _quoter_address) payable {
-        token_address = _token_address;
+    constructor(
+        address _tokenAddress,
+        address _tokenIn,
+        address _swapRouterAddress,
+        address _stakingAddress,
+        address _quoterAddress
+    ) payable {
+        tokenAddress = _tokenAddress;
         tokenIn = _tokenIn;
-        TokenContract = DentacoinToken(_token_address);
-        SwapRouter = ISwapRouter(_swap_router_address);
-        StakingContract = StakingProgram(_staking_address);
-        Quoter = IQuoter(_quoter_address);
+        tokenContract = DentacoinToken(_tokenAddress);
+        swapRouter = ISwapRouter(_swapRouterAddress);
+        stakingContract = StakingProgram(_stakingAddress);
+        quoter = IQuoter(_quoterAddress);
 
-        TokenContract.approve(_staking_address, 115792089237316195423570985008687907853269984665640564039457584007913129639935);
+        tokenContract.approve(_stakingAddress, 115792089237316195423570985008687907853269984665640564039457584007913129639935);
     }
 
     // ==================================== EVENTS ====================================
-    event boughtAndStaked(address indexed _staker, uint256 _tokenIn, uint256 _tokenOut);
+    event BoughtAndStaked(address indexed _staker, uint256 _tokenIn, uint256 _tokenOut);
     // ==================================== /EVENTS ====================================
 
     // ==================================== MODIFIERS ====================================
@@ -135,8 +179,12 @@ contract StakingAccelerator is Ownable, SafeMath {
         }
     }
 
-    function setContractParams(uint24 _uniswapFee, uint16 _slippage, uint256 _incentivePercentage) external onlyOwner {
-        require(_slippage < 100, "ERROR: slippage is not valid.");
+    function setContractParams(
+        uint24 _uniswapFee,
+        uint16 _slippage,
+        uint256 _incentivePercentage
+    ) external onlyOwner {
+        require(_slippage < 100, "ERROR: INVALID_SLIPPAGE");
 
         uniswapFee = _uniswapFee;
         slippage = _slippage;
@@ -146,71 +194,35 @@ contract StakingAccelerator is Ownable, SafeMath {
 
     // ===================================== CONTRACT BODY =====================================
     function buyAndStake() external payable checkIfContractStopped {
-        require(msg.value > 0, "ERROR: Not enough ETH.");
+        require(msg.value > 0, "ERROR: NOT_ENOUGH_ETH");
 
         if (incentivePercentage > 0) {
-            require(TokenContract.balanceOf(address(this)) > 0, "ERROR: not enough token balance.");
+            require(tokenContract.balanceOf(address(this)) > 0, "ERROR: NOT_ENOUGH_TOKEN_BALANCE");
         }
 
         // calculate Uniswap amountOutMinimum to prevent sandwich attack
-        uint256 quoteAmountOut = Quoter.quoteExactInputSingle(tokenIn, token_address, uniswapFee, msg.value, 0);
-        require(quoteAmountOut > 0, "ERROR: failed predicting uniswap trade outcome.");
+        uint256 quoteAmountOut = quoter.quoteExactInputSingle(tokenIn, tokenAddress, uniswapFee, msg.value, 0);
+        require(quoteAmountOut > 0, "ERROR: FAILED_quoteAmountOut");
         uint256 amountOutMinimum = div(mul(quoteAmountOut, sub(100, slippage)), 100);
 
         // Uniswap purchase
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({tokenIn: tokenIn, tokenOut: token_address, fee: uniswapFee, recipient: address(this), amountIn: msg.value, amountOutMinimum: amountOutMinimum, sqrtPriceLimitX96: 0});
-        uint256 amountOut = SwapRouter.exactInputSingle{value: msg.value}(params);
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({tokenIn: tokenIn, tokenOut: tokenAddress, fee: uniswapFee, recipient: address(this), amountIn: msg.value, amountOutMinimum: amountOutMinimum, sqrtPriceLimitX96: 0});
+        uint256 amountOut = swapRouter.exactInputSingle{value: msg.value}(params);
 
         // Add incentive to the buyers amount
         uint256 incentiveAmount = div(mul(amountOut, incentivePercentage), 100);
         amountOut = add(amountOut, incentiveAmount);
 
         // Stake the amount as from the name of the buyer
-        StakingContract.whitelistedStake(amountOut, msg.sender);
+        stakingContract.whitelistedStake(amountOut, msg.sender);
 
-        emit boughtAndStaked(msg.sender, msg.value, amountOut);
+        emit BoughtAndStaked(msg.sender, msg.value, amountOut);
     }
 
     function withdrawTokens() external onlyOwner {
-        TokenContract.transfer(owner, TokenContract.balanceOf(address(this)));
+        tokenContract.transfer(owner, tokenContract.balanceOf(address(this)));
     }
     // ===================================== /CONTRACT BODY =====================================
-}
-
-interface DentacoinToken {
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function balanceOf(address account) external view returns (uint256);
-}
-
-interface ISwapRouter {
-    function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
-
-    struct ExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
-    }
-}
-
-interface IQuoter {
-    function quoteExactInputSingle(
-        address tokenIn,
-        address tokenOut,
-        uint24 fee,
-        uint256 amountIn,
-        uint160 sqrtPriceLimitX96
-    ) external returns (uint256 amountOut);
-}
-
-interface StakingProgram {
-    function whitelistedStake(uint256 _tokens_amount, address _staker) external;
 }
 
 // MN bby ¯\_(ツ)_/¯
